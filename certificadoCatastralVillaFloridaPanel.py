e117e74b84a88bd3c31874ab9f978954e6789c86
fdiaz <fdiaz@Waterfall>
2022-06-08 10:03:51 +0200

    Improved

--- certificadoCatastralVillaFloridaPanel.py
+++ certificadoCatastralVillaFloridaPanel.py
@@ -2,6 +2,7 @@
 
 import gvsig
 import os
+from ConfigParser import SafeConfigParser
 
 from gvsig.uselib import use_plugin
 use_plugin("org.gvsig.app.document.layout2.app.mainplugin")
@@ -10,14 +11,44 @@ use_plugin("org.gvsig.pdf.app.mainplugin")
 from org.gvsig.tools import ToolsLocator
 from org.gvsig.pdf.swing.api import PDFSwingLocator, PDFViewer
 from org.gvsig.pdf.lib.api import PDFLocator
-from org.gvsig.tools.swing.api import ToolsSwingLocator
+from org.gvsig.tools.swing.api import ToolsSwingLocator, ToolsSwingUtils
 from org.gvsig.tools.swing.api.windowmanager import WindowManager
 from java.io import File, FileInputStream
 from gvsig.libs.formpanel import FormPanel, FormComponent
 from gvsig import currentLayer
 from org.gvsig.app.project.documents.layout.gui import DefaultLayoutPanel
+from org.gvsig.fmap.mapcontext import MapContextLocator
 
+from org.gvsig.scripting import ScriptingLocator
 
+def getDataFolder():
+  return ScriptingLocator.getManager().getDataFolder("CertificadoCatastralVillaFlorida").getAbsolutePath()
+
+def getConfigFilePath():
+  ap = ScriptingLocator.getManager().getDataFolder("CertificadoCatastralVillaFlorida").getAbsolutePath()
+  configFile = os.path.join(ap, "config.ini")
+  return configFile
+  
+def readConfigFile():
+  parser = SafeConfigParser()
+  configFile = getConfigFilePath()
+  if os.path.exists(configFile):
+    parser.read(configFile) 
+    items = dict(parser.items('preferences'))
+    return items
+  else:
+    writeConfigFile(imagesfolder = "")
+    return readConfigFile()
+
+def writeConfigFile(**args):
+  parser = SafeConfigParser()
+  configFile = getConfigFilePath()
+  parser.add_section('preferences')
+  for k,v in args.items():
+    parser.set('preferences', k, v)
+  cfgfile = open(configFile,'w')
+  parser.write(cfgfile)
+  cfgfile.close()
 
 
 class CertificadoCatastralVillaFloridaPanel(FormPanel):
@@ -25,7 +56,6 @@ class CertificadoCatastralVillaFloridaPanel(FormPanel):
     FormPanel.__init__(self, gvsig.getResource(__file__,"certificadoCatastralVillaFloridaPanel.xml"))
     i18n = ToolsLocator.getI18nManager()
     swingManager = ToolsSwingLocator.getToolsSwingManager()
-    self.setPreferredSize(480,180)
     self.layer = layer
 
     self.pickerFolder = swingManager.createFolderPickerController(
@@ -36,6 +66,10 @@ class CertificadoCatastralVillaFloridaPanel(FormPanel):
     swingManager.translate(self.lblImagesFolder)
     swingManager.translate(self.btnGenerateCertificate)
 
+    config = readConfigFile()
+    self.txtImagesFolder.setText(config.get("imagesfolder",""))
+    ToolsSwingUtils.ensureRowsCols(self.asJComponent(), 7, 70, 10, 100)
+
 
   def btnGenerateCertificate_click(self, event):
     layout = self.loadTemplate()
@@ -45,8 +79,9 @@ class CertificadoCatastralVillaFloridaPanel(FormPanel):
     if selection.getSelectedCount() != 1:
       return
 
-    images = self.getImages()
-    for feature in selection:
+    feature = selection.first()
+    if feature:
+      images = self.getImages(feature)
       #feature = None #Coger la feature seleccionada de la vista
       geometry = feature.getDefaultGeometry()
       encuadre = geometry.buffer(2).getEnvelope()
@@ -66,8 +101,11 @@ class CertificadoCatastralVillaFloridaPanel(FormPanel):
         elif elemento.getTag() == "ATRIBUTO":
           name = elemento.getText().get(0)
           elemento.clearText() 
-          elemento.addText(feature.getString(name))
-
+          value = feature.getString(name)
+          if value:
+            elemento.addText(value)
+          else:
+            elemento.addText("")
         elif elemento.getTag() == "IMAGEN0":
           elemento.setImage(images[0])
           imageFrames[0] = elemento
@@ -79,7 +117,6 @@ class CertificadoCatastralVillaFloridaPanel(FormPanel):
         elif elemento.getTag() == "IMAGEN2":
           elemento.setImage(images[2])
           imageFrames[2] = elemento
-      break
     for n in range(0,3):
       if images[n] == None:
         context.delFFrame(imageFrames[n])
@@ -88,7 +125,7 @@ class CertificadoCatastralVillaFloridaPanel(FormPanel):
     layout.getLayoutControl().getLayoutDraw().initialize()
 
     tmpFile = ToolsLocator.getFoldersManager().getUniqueTemporaryFile("informe.pdf")
-    
+    self.highlightFeature(feature,mapContext)
     layout.layoutToPDF(tmpFile);
 
     pdfDoc = PDFLocator.getPDFManager().createPDFDocument(tmpFile)
@@ -102,22 +139,25 @@ class CertificadoCatastralVillaFloridaPanel(FormPanel):
       i18n.getTranslation("_Report"), 
       WindowManager.MODE.WINDOW
       ) 
+    writeConfigFile(imagesfolder=self.txtImagesFolder.getText())
 
-  def getImages(self):
+  def getImages(self, feature):
     toolsSwingManager = ToolsSwingLocator.getToolsSwingManager()
+    images = [None] * 3
     folder=self.pickerFolder.get()
-    images = list()
     if not folder:
-      # raturn a list with 3 None
-      return [None] * 3
+      # return a list with 3 None
+      return images
     basePath = folder.getAbsolutePath()
-    imagesPath = os.listdir(basePath)
-    for n in range(0,3):
-      if len(imagesPath) <= n:
-        images.append(None)
-        continue
-      image = toolsSwingManager.createSimpleImage(basePath+os.sep+imagesPath[n])
-      images.append(image.getBufferedImage())
+    n = 0;
+    for s in ["IMAGEN1","IMAGEN2","IMAGEN3"]:
+      featImageString = feature.getStringOrDefault(s, None)
+      if featImageString:
+        imagePath = os.path.join(basePath, featImageString).replace("\\","/")
+        if os.path.exists(imagePath):
+          image = toolsSwingManager.createSimpleImage(File(imagePath))
+          images[n] = image.getBufferedImage()
+      n = n+1
     return images
 
   def loadTemplate(self):
@@ -129,6 +169,22 @@ class CertificadoCatastralVillaFloridaPanel(FormPanel):
     layout = persistenceManager.create(persistentState)
     return layout
 
+  def highlightFeature(self, feature, mapContext):
+    graphics = mapContext.getGraphicsLayer()
+    graphics.clearAllGraphics()
+    graphics.clearAllSymbols()
+    f = os.path.join(getDataFolder(), "selection.gvssym")
+    sym = None
+    if os.path.exists(f):
+      sym = MapContextLocator.getSymbolManager().loadSymbol(File(f))
+    if sym == None:
+      sym = self.layer.getLegend().getSymbolByFeature(feature).getSymbolForSelection()
+      setTransparency = getattr(sym, 'setTransparency', None)
+      if setTransparency != None:
+        setTransparency(0.5)
+    n = graphics.addSymbol(sym)
+    graphics.addGraphic("CertificadoCatastralVillaFlorida", feature.getDefaultGeometry(), n)
+    
 
 def main(*args):
 
@@ -136,6 +192,3 @@ def main(*args):
     swingManager = ToolsSwingLocator.getToolsSwingManager()
     f = CertificadoCatastralVillaFloridaPanel(currentLayer())
     f.showWindow(i18n.getTranslation("_Certificado_Catastral_Villa_Florida"))
-
-    print "hola mundo"
-    pass
